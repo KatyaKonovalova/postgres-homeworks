4180b1fa117adb905c549325ca211b85db041ca6
konov <konovalova_ekaterina_a@mail.ru>
2024-04-24 18:55:15 +0300

    done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,34 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customer_id char PRIMARY KEY,
+    company_name varchar(50),
+    contact_name varchar(100)
+);
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+    first_name varchar(50),
+    las_name varchar,
+    title varchar(100),
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar,
+	employee_id int,
+	order_date date,
+	ship_city varchar(50)
+);
+
+ALTER TABLE orders
+ALTER COLUMN customer_id SET DATA TYPE varchar,
+ADD CONSTRAINT emploeey_fk FOREIGN KEY (employee_id)
+REFERENCES employees(employee_id),
+ADD CONSTRAINT emploeey_fk FOREIGN KEY (employee_id)
+REFERENCES employees(employee_id);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='LEGENDofTHEseeker1!1')
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            with open('north_data\\customers_data.csv', 'r', encoding='utf-8') as csvfile:
+                pass_line = next(csvfile)
+                data = csv.reader(csvfile)
+                for row in data:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+
+            with open('north_data\\employees_data.csv', 'r', encoding='utf-8') as csvfile:
+                pass_line = next(csvfile)
+                data = csv.reader(csvfile)
+                for row in data:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', row)
+
+            with open('north_data\\orders_data.csv', 'r', encoding='utf-8') as csvfile:
+                pass_line = next(csvfile)
+                data = csv.reader(csvfile)
+                for row in data:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+finally:
+    conn.close()
