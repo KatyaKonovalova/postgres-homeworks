15caa97c5bcec9106c161d67a81133045a3c9031
konov <konovalova_ekaterina_a@mail.ru>
2024-04-28 14:18:02 +0300

    comments have been corrected

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE customers
 (
-	customer_id char PRIMARY KEY,
+	customer_id char(5) PRIMARY KEY,
     company_name varchar(50),
     contact_name varchar(100)
 );
@@ -11,7 +11,7 @@ CREATE TABLE employees
 (
 	employee_id int PRIMARY KEY,
     first_name varchar(50),
-    las_name varchar,
+    last_name varchar,
     title varchar(100),
     birth_date date,
     notes text
@@ -20,15 +20,10 @@ CREATE TABLE employees
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id varchar,
+	customer_id char(5),
 	employee_id int,
 	order_date date,
-	ship_city varchar(50)
+	ship_city varchar(50),
+    CONSTRAINT customer_fk FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    CONSTRAINT employee_fk FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
-
-ALTER TABLE orders
-ALTER COLUMN customer_id SET DATA TYPE varchar,
-ADD CONSTRAINT customer_fk FOREIGN KEY (customer_id)
-REFERENCES customers(customer_id),
-ADD CONSTRAINT emploeey_fk FOREIGN KEY (employee_id)
-REFERENCES employees(employee_id);
--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,7 @@ import csv
 import os
 
 import psycopg2
-conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='LEGENDofTHEseeker1!1')
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
 
 try:
     with conn:
@@ -13,18 +13,29 @@ try:
                 pass_line = next(csvfile)
                 data = csv.reader(csvfile)
                 for row in data:
-                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (row[0],
+                                                                              row[1],
+                                                                              row[2]))
 
-            with open(os.path.join('north_data/customers_data.csv'), 'r', encoding='utf-8') as csvfile:
+            with open(os.path.join('north_data/employees_data.csv'), 'r', encoding='utf-8') as csvfile:
                 pass_line = next(csvfile)
                 data = csv.reader(csvfile)
                 for row in data:
-                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', row)
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (row[0],
+                                                                                          row[1],
+                                                                                          row[2],
+                                                                                          row[3],
+                                                                                          row[4],
+                                                                                          row[5]))
 
-            with open(os.path.join('north_data/customers_data.csv'), 'r', encoding='utf-8') as csvfile:
+            with open(os.path.join('north_data/orders_data.csv'), 'r', encoding='utf-8') as csvfile:
                 pass_line = next(csvfile)
                 data = csv.reader(csvfile)
                 for row in data:
-                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (row[0],
+                                                                                   row[1],
+                                                                                   row[2],
+                                                                                   row[3],
+                                                                                   row[4]))
 finally:
     conn.close()
